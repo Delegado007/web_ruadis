9984332e70153da3a17c503d52cc8754838220fa
Delegado <maxharper2013@gmail.com>
2022-03-18 11:54:04 -0300

    probando los temas para la pagina

--- src/ItemMain.jsx
+++ src/ItemMain.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./image/logoPdf.jpg";
+import logo from "./image/logoPdf2.png";
 
 function ItemMain({ titulo, ano, carillas }) {
   return (
-    <div className="card card-side bg-base-100 shadow-xl m-4 w-80">
-      <figure className="w-40">
-        <img src={logo} alt="Movie"></img>
+    <div className="card card-side bg-base-100 shadow-xl m-4 w-96">
+      <figure className=" bg-slate-700">
+        <img className="w-36 px-2" src={logo} alt="Movie"></img>
       </figure>
       <div className="card-body">
         <h2 className="card-title">{titulo}</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   daisyui: {
-    themes: ["bumblebee"],
+    themes: ["dark"],
   },
 
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        body: { backgroundColor: "#494949" },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+      });
+    }),
+  ],
 };
