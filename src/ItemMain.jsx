04ffa4ee128f50d64cc1ee8f31fd43bd87a9169e
Delegado <maxharper2013@gmail.com>
2022-03-18 13:18:04 -0300

    antes de romper el menu

--- src/ItemMain.jsx
+++ src/ItemMain.jsx
@@ -3,7 +3,7 @@ import logo from "./image/logoPdf2.png";
 
 function ItemMain({ titulo, ano, carillas }) {
   return (
-    <div className="card card-side bg-base-100 shadow-xl m-4 w-96">
+    <div className="card card-side bg-base-100 shadow-xl m-4 w-96 text-slate-200">
       <figure className=" bg-slate-700">
         <img className="w-36 px-2" src={logo} alt="Movie"></img>
       </figure>
@@ -12,7 +12,7 @@ function ItemMain({ titulo, ano, carillas }) {
         <p>Año: {ano}</p>
         <p>Carillas: {carillas}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Comprar</button>
+          <button className="btn btn-success">Pedir</button>
         </div>
       </div>
     </div>
