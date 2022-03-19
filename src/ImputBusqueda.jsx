8fce677163738e424be8d52c795dcd0f242c78d8
Delegado <maxharper2013@gmail.com>
2022-03-19 10:49:44 -0300

    modificacion imput busqueda

--- src/ImputBusqueda.jsx
+++ src/ImputBusqueda.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function ImputBusqueda() {
   return (
-    <div className="navbar bg-zinc-800">
+    <div className="bg-zinc-800">
       <input
         type="text"
         placeholder="Busca aquí"
-        class="input input-bordered input-accent w-full max-w-xs"
+        className="input input-bordered input-accent w-full max-w-xs"
       ></input>
     </div>
   );
